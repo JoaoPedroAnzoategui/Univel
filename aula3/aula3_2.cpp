6d74950575253952a113cecf7cde76b4a9a2e3d7
João Pedro Anzoategui <joaopedroanzoategui@gmail.com>
2026-03-10 22:01:29 -0300

    Segunda atividade realizada em sala utilizando vscode, utilizando linguagem C++
    Testado

---
+++ aula3/aula3_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num1, num2;
+    //Recebendo os dados
+    cout << "Digite o primeiro numero: "; //count mostra
+    //mensagens ao usuario
+    cin >> num1; //cin libera teclado e recolhe o valor digitado 
+    //scanf('%f', &num1); linha do c
+
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+
+    return 0;
+}
