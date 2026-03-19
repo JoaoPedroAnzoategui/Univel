cb963588877832201631b305b46419beb35975fa
João Pedro Anzoategui <joaopedroanzoategui@gmail.com>
2026-03-19 20:17:23 -0300

    Aula 4, Atividade 7 realizada em sala

---
+++ aula4/aula4_7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int x = 10, y = 2, z=0;
+    cout << "o valor de x = " << x << "\n";
+    cout << "o valor de y = " << y << "\n";
+    x = x * -1;
+    cout << "o valor de x negativo = " << x << "\n";
+    return 0;
+}
